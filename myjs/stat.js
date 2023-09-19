9c2827f92dc49514b9516a5ef2c576cb6aad16c9
cxx0515 <2539086@qq.com>
2023-09-19 08:51:03 +0000

    update myjs/stat.js.
    Signed-off-by: cxx0515 <2539086@qq.com>

--- myjs/stat.js
+++ myjs/stat.js
@@ -1,7 +1,7 @@
 document.oncontextmenu = function () { return false; };//禁止右键
 document.onkeydown = function () {
 	if (window.event && window.event.keyCode == 123) {
-		alert('请尊重劳动成果！cxx0515.gitee.io');
+		alert('请尊重劳动成果！by:cxx0515');
 		event.keyCode = 0;
 		event.returnValue = false;
 		 return false;
